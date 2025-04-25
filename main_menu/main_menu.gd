770d43880d4f8afe946929343add1a1e8aa53cfe
Katharina Bork <katha.bork75@gmail.com>
2025-04-25 23:32:27 +0200

    finished menu (for now)
    added:
    - better scene layers
    - center container for centering buttons
    - scaling buttons
    - vbox container for spacing and sorting
    - added focus and navigation with arrow keys

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,5 +1,8 @@
-extends Node2D
+extends Control
 
+func _ready() -> void:
+	var start_button = get_node("CenterContainer/VBoxContainer/start_button")
+	start_button.grab_focus()
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
